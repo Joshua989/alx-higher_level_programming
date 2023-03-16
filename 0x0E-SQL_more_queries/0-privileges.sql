b06f6d7a08f2b998664dba9ace5e861f01262ae7
Joshua989 <josipoli0222@gmail.com>
2023-03-16 15:44:30 -0700

    go

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,7 +1,3 @@
---Output the privileges for user_0d_1
-echo "SHOW GRANTS FOR 'user_0d_1'@'localhost';" | mysql -h localhost -u root -p
-
---Output the privileges for user_0d_2
-echo "SHOW GRANTS FOR 'user_0d_2'@'localhost';" | mysql -h localhost -u root -p
-
-
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,11 +1,3 @@
---Check if the user already exists
-if mysql -h localhost -u root -p -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'user_0d_1');"; then
-    echo "User user_0d_1 already exists, skipping creation."
-else
-    --Create the user with all privileges and set the password
-    echo "CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';" | mysql -h localhost -u root -p
-    echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;" | mysql -h localhost -u root -p
-    echo "FLUSH PRIVILEGES;" | mysql -h localhost -u root -p
-    echo "User user_0d_1 created successfully."
-fi
-
+-- creates the MySQL server user user_0d_1 and grant all priviledges
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
