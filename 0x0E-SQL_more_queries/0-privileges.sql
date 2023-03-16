3a7f6b656a2168b33fb3826f8b0df53af6071f89
Joshua989 <josipoli0222@gmail.com>
2023-03-15 17:28:05 -0700

    vuhs

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,7 @@
-echo "CREATE USER 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
-echo "SHOW GRANTS FOR 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+--Output the privileges for user_0d_1
+echo "SHOW GRANTS FOR 'user_0d_1'@'localhost';" | mysql -h localhost -u root -p
+
+--Output the privileges for user_0d_2
+echo "SHOW GRANTS FOR 'user_0d_2'@'localhost';" | mysql -h localhost -u root -p
+
 
