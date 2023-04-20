8557eaf0ff17b684569cfbfa430a8b21b82699e7
Joshua989 <josipoli0222@gmail.com>
2023-04-20 02:36:48 -0700

    bajiaiq

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
 
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+// Define variables for three programming languages
+let c = "C is fun";
+let python = "Python is cool";
+let js = "JavaScript is amazing";
+
+// Print the variables to the console
+console.log(c);
+console.log(python);
+console.log(js);
+
